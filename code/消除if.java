d529b51cee81348dbccd8d52248db50e1085381b
周游 <mickey_zhouyou@yeah.net>
2014-09-10 11:58:42 +0800

    commit;

--- code/消除if.java
+++ code/消除if.java
@@ -6,23 +6,17 @@ public class 消除if {
 		T getData();
 	}
 
-	private Holder<Date> data = new Holder<Date>() {
-		public Date getData() {
-			final Date d = new Date();
-			data = new Holder<Date>() {
-				public Date getData() {
-					return d;
-				}
-			};
-			return d;
-		}
+	private Holder<Date> data = () -> {
+		Date d = new Date();
+		data = () -> d;
+		return d;
 	};
 
 	public Date getDate() {
 		return data.getData();
 	}
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception {
 		消除if a = new 消除if();
 		System.out.println(a.getDate());
 		System.out.println(a.getDate());
