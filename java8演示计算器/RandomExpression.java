33ce754f0aa2f8829fa0a0a2d8158f7977f1c05e
周游 <mickey_zhouyou@yeah.net>
2015-02-08 21:19:50 +0800

    commit;

--- java8演示计算器/RandomExpression.java
+++ java8演示计算器/RandomExpression.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 public class RandomExpression {
 
 	private static Random r = new Random();
+	private static final char[] ops = { '+', '-', '*', '/', };
 
 	public static void main(String[] args) throws Exception {
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter("exps"));) {
@@ -19,13 +20,8 @@ public class RandomExpression {
 
 	private static String genExp() {
 		int len = 2 + r.nextInt(5);
-		return Stream.generate(() -> r.nextInt(100)).limit(len).map(i -> "" + genop() + i)
+		return Stream.generate(() -> r.nextInt(100)).limit(len).map(i -> "" + ops[r.nextInt(4)] + i)
 				.collect(Collectors.joining()).substring(1);
 	}
 
-	private static final char[] ops = { '+', '-', '*', '/', };
-
-	private static char genop() {
-		return ops[r.nextInt(4)];
-	}
 }
