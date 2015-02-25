e7b617f666325f7d8d37e82be2e3dfb29d8422c2
周游 <mickey_zhouyou@yeah.net>
2015-02-25 10:38:04 +0800

    commit;

--- 牛顿法求微分/Test.java
+++ 牛顿法求微分/Test.java
@@ -1,6 +1,8 @@
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -17,7 +19,7 @@ public class Test extends JFrame {
 		double f(double d);
 	}
 
-	private static final double dx = 0.05; // 如果太小影响绘图的速度，太大就不精确。
+	private static final double dx = 0.01;
 	// 求任意函数的微分函数
 	private static final Function<Fun, Fun> fdx = fun -> x -> (fun.f(x + dx) - fun.f(x)) / dx;
 	private static final Fun f1 = x -> Math.sin(x * x / 4);
@@ -31,13 +33,14 @@ public class Test extends JFrame {
 		super.setBounds(10, 50, winWidth, winHeight);
 		super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		super.setResizable(false);
+		initBf();
 		super.setVisible(true);
 
 		final Container cp = super.getContentPane();
-		final int leftLimit = (winWidth - cp.getWidth()) / 2;
-		final int rightLimit = winWidth - leftLimit - 1;
-		final int topLimit = winHeight - cp.getHeight() - leftLimit;
-		final int bottomLimit = winHeight - leftLimit - 1;
+		final int leftLimit = -cp.getWidth() - (winWidth - cp.getWidth()) / 2;
+		final int rightLimit = -(winWidth - cp.getWidth()) / 2 - 1;
+		final int topLimit = -cp.getHeight() - (winWidth - cp.getWidth()) / 2;
+		final int bottomLimit = rightLimit;
 
 		MouseAdapter ma = new MouseAdapter() {
 			private int x;
@@ -62,50 +65,58 @@ public class Test extends JFrame {
 		};
 		super.addMouseListener(ma);
 		super.addMouseMotionListener(ma);
+
 	}
 
 	private final int winWidth = 1000;
 	private final int winHeight = (int) (winWidth * (Math.sqrt(5) - 1) / 2);
 	private final int bs = 100;
-	private int xoffset = winWidth - winHeight;
-	private int yoffset = winHeight / 2;
+	private int xoffset = -winHeight;
+	private int yoffset = -winHeight / 2;
+
+	private BufferedImage bf;
 
 	@Override
 	public void paint(Graphics g) {
-		BufferedImage bf = new BufferedImage(winWidth, winHeight, BufferedImage.TYPE_INT_RGB);
-		Graphics bg = bf.createGraphics();
-		bg.fillRect(0, 0, winWidth, winHeight);
+		g.drawImage(bf, xoffset, yoffset, null);
+	}
+
+	private void initBf() {
+		int bfWidth = winWidth * 2;
+		int bfHeight = winHeight * 2;
+		bf = new BufferedImage(bfWidth, bfHeight, BufferedImage.TYPE_INT_RGB);
+		Graphics2D bg = (Graphics2D) bf.createGraphics();
+		bg.fillRect(0, 0, bfWidth, bfHeight);
 
 		bg.setColor(Color.LIGHT_GRAY);
 		int step = 4;
-		Stream.iterate(0, i -> i + step).limit(winWidth / step)
-				.forEach(i -> bg.drawLine(i, yoffset, i + step / 2, yoffset));
-		Stream.iterate(0, i -> i + step).limit(winHeight / step)
-				.forEach(i -> bg.drawLine(xoffset, i, xoffset, i + step / 2));
+		Stream.iterate(0, i -> i + step).limit(bfWidth / step)
+				.forEach(i -> bg.drawLine(i, winHeight, i + step / 2, winHeight));
+		Stream.iterate(0, i -> i + step).limit(bfHeight / step)
+				.forEach(i -> bg.drawLine(winWidth, i, winWidth, i + step / 2));
 
-		for (double x = 0 - xoffset; x < winWidth; x += dx) {
+		bg.setStroke(new BasicStroke(2f));
+		for (double x = 0 - winWidth; x < bfWidth; x += dx) {
 			paintStep(bg, Color.RED, f1, x);
 			paintStep(bg, Color.BLUE, f2, x);
 		}
-
-		g.drawImage(bf, 0, 0, null);
 	}
 
-	private void paintStep(Graphics g, Color color, Fun f, double x) {
+	private void paintStep(Graphics2D g, Color color, Fun f, double x) {
 		g.setColor(color);
 		double y = f.f(x);
 		if (Double.isNaN(y) || Double.isInfinite(y))
 			return;
 		int x1 = (int) (x * bs);
-		int y1 = (int) (yoffset + (y * bs) * -1);
+		int y1 = (int) (winHeight + (y * bs) * -1);
 		x += dx;
 		y = f.f(x);
 		if (Double.isNaN(y) || Double.isInfinite(y))
 			return;
 		int x2 = (int) (x * bs);
-		int y2 = (int) (yoffset + (y * bs) * -1);
-		if ((y1 < 0 && y2 < 0) || (winHeight < y1 && winHeight < y2))
+		int y2 = (int) (winHeight + (y * bs) * -1);
+		if ((y1 < -winWidth && y2 < -winWidth) || (winHeight * 2 < y1 && winHeight * 2 < y2))
 			return;
-		g.drawLine(xoffset + x1, y1, xoffset + x2, y2);
+		g.drawLine(winWidth + x1, y1, winWidth + x2, y2);
 	}
 }
